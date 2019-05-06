5989a336de98411799253c5b6cda1dbeb51dd89a
minhye-lee <dlalsgp001@naver.com>
2019-05-06 18:22:33 +0900

    add inputwithlabel componenet

---
+++ src/component/InputWithLabel.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import {Col, Form, Row} from "react-bootstrap";
+
+const InputWithLabel = ({label, ...rest}) => (
+    <Form>
+        <Form.Group as={Row}>
+            <Form.Label column sm={2}>
+                {label}
+            </Form.Label>
+            <Col sm={10}>
+                <Form.Control {...rest} />
+            </Col>
+        </Form.Group>
+    </Form>
+)
+
+export default InputWithLabel
--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -1,39 +1,15 @@
 import React, { Component } from 'react'
-import {Button, Col, Form, Row} from "react-bootstrap";
+import {Button} from "react-bootstrap";
+import InputWithLabel from './InputWithLabel'
 
 class SignUp extends Component {
     render = () => {
         return (
             <div>
                 <h4>회원가입</h4>
-                    <Form>
-                        <Form.Group as={Row} controlId="formHorizontalEmail">
-                            <Form.Label column sm={2}>
-                                Email
-                            </Form.Label>
-                            <Col sm={10}>
-                                <Form.Control type="email" placeholder="Email" />
-                            </Col>
-                        </Form.Group>
-                        <Form.Group as={Row} controlId="formHorizontalUsername">
-                            <Form.Label column sm={2}>
-                                Username
-                            </Form.Label>
-                            <Col sm={10}>
-                                <Form.Control type="username" placeholder="Username" />
-                            </Col>
-                    </Form.Group>
-                    <Form>
-                        <Form.Group as={Row} controlId="formHorizontalPassword">
-                            <Form.Label column sm={2}>
-                                Password
-                            </Form.Label>
-                            <Col sm={10}>
-                                <Form.Control type="password" placeholder="Password" />
-                            </Col>
-                        </Form.Group>
-                    </Form>
-                </Form>
+                <InputWithLabel label="Email" type="Email" placeholder="Email"/>
+                <InputWithLabel label="Username" type="Username" placeholder="Username"/>
+                <InputWithLabel label="Password" type="Password" placeholder="Password"/>
                 <Button variant="secondary" size="lg" block>
                     Submit
                 </Button>
